3f1b70fea7150ec543a79d511c10b3b79c8f92a4
Sahana Balram <sahana2008balram@gmail.com>
2018-03-28 14:55:44 -0700

    Create zoom effect

--- map.js
+++ map.js
@@ -10,5 +10,28 @@ $(document).ready(function(){
     var svg = d3.select("body").append("svg").attr("width",width).attr("height",height)
 
     // append the data 
-    svg.append("rect").attr("width",width).attr("height",height)
+    svg.append("rect").attr("width",width).attr("height",height).attr("fill","white");
+    // create an empty placeholder to hold the geomerty elements in the SVG
+    var g = svg.append("g");
+    // add the d3.json to load the topojson data from the url given from freecodecamp
+    d3.json("https://d3js.org/world-50m.v1.json",function(error,data){
+        if(error){
+            console.log(error);
+        }
+
+        g.append("path").datum(topojson.feature(data,data.objects.countries)).attr("d",path);
+
+        // create the zoom effect
+
+        var zoom = d3.behavior.zoom()
+        .on("zoom",function(){
+            g.attr("transform","translate(" + d3.event.scale + ")");
+            g.selectAll("path").attr("d",path.projection(projection));
+        });
+
+        // append zoom effect to the svg canvas
+        svg.append("zoom");
+
+        // load data from json file
+    })
 });
