b6106fc18d64bde80a4d057a320b441a56805622
Sahana Balram <sahana2008balram@gmail.com>
2018-03-28 14:42:51 -0700

    Create SVG Canvas

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
-})
+    // declare width and height of the whole map visualization
+    var width = 1000;
+    var height = 400;
+// D3 has built in functionality and hence projection is being used to read the geoJSON data
+    var projection = d3.geo.equirectangular();
+    // lat and lon coordinated gets displayed on the screen
+    var path = d3.geo.path().projection(projection);
+    // following elements are added to create the SVG canvas
+    var svg = d3.select("body").append("svg").attr("width",width).attr("height",height)
+
+    // append the data 
+    svg.append("rect").attr("width",width).attr("height",height)
+});
